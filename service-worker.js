82d1124b5b57bf5cc982b0eb044053bae7d7f854
Mati-Quiroz <matiaseliecerquiroz@gmail.com>
2026-06-06 21:05:18 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "ptla-practice-v1";
+
+const urlsToCache = [
+  "/PTLA-Prep/",
+  "/PTLA-Prep/index.html",
+  "/PTLA-Prep/style.css",
+  "/PTLA-Prep/app.js",
+  "/PTLA-Prep/banco_preguntas_ptla.json",
+  "/PTLA-Prep/PTLA PRACTICE.png",
+  "/PTLA-Prep/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
