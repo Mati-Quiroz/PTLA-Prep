f32e1c451f0fa95cf7f2694652002ac88379647a
MQ Vector Foundry <matiaseliecerquiroz@gmail.com>
2026-06-07 12:21:57 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,11 @@
-const CACHE_NAME = "ptla-practice-v103";
-
-const urlsToCache = [
-  "/PTLA-Prep/",
-  "/PTLA-Prep/index.html",
-  "/PTLA-Prep/style.css",
-  "/PTLA-Prep/app.js",
-  "/PTLA-Prep/banco_preguntas_ptla.json",
-  "/PTLA-Prep/icon-512.png",
-  "/PTLA-Prep/manifest.json"
-];
-
-self.addEventListener("install", event => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener("activate", event => {
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.map(key => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      }))
+      Promise.all(keys.map(key => caches.delete(key)))
     ).then(() => self.clients.claim())
   );
 });
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const clone = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
 });
